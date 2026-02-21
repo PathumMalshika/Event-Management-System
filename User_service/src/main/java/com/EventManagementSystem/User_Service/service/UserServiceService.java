78496ce7e09cadf399231815316cad33a8ac8b67
NethmiJayasinghee <ndhananjana98@gmail.com>
2026-02-21 00:12:44 -0800

    update UserServiceService  interface

--- User_service/src/main/java/com/EventManagementSystem/User_Service/service/UserServiceService.java
+++ User_service/src/main/java/com/EventManagementSystem/User_Service/service/UserServiceService.java
@@ -1,4 +1,11 @@
 package com.EventManagementSystem.User_Service.service;
 
+import com.EventManagementSystem.User_Service.entity.dto.UserServiceDto;
+import java.util.List;
+
 public interface UserServiceService {
+    UserServiceDto createUser(UserServiceDto dto);
+    List<UserServiceDto> getAllUsers();
+    UserServiceDto updateUser(Long id, UserServiceDto dto);
+    void deleteUser(Long id);
 }
