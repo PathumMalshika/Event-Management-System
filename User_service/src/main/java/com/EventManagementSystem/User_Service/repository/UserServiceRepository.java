ee9dfbc71fabd5b9b40c205dbcd66bb5a9a1cff8
NethmiJayasinghee <ndhananjana98@gmail.com>
2026-02-21 00:11:47 -0800

    update UserServiceRepository interface

--- User_service/src/main/java/com/EventManagementSystem/User_Service/repository/UserServiceRepository.java
+++ User_service/src/main/java/com/EventManagementSystem/User_Service/repository/UserServiceRepository.java
@@ -1,4 +1,9 @@
 package com.EventManagementSystem.User_Service.repository;
 
-public interface UserServiceRepository {
+import com.EventManagementSystem.User_Service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserServiceRepository extends JpaRepository<User, Long> {
 }
