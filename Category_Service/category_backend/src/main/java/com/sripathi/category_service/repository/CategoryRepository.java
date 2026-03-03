8f574a40532769f7ca91f9aac9ac123b8360f0b7
SripathiUdayakantha <sripathiudayakantha@gmail.com>
2026-03-04 04:11:22 +0530

    Delete Category_Service directory

--- Category_Service/category_backend/src/main/java/com/sripathi/category_service/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sripathi.category_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sripathi.category_service.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
