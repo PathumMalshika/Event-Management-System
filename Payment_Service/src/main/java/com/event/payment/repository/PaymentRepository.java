c3d678e3e25ab67d55c09409fd3e07a4298a9856
Sajith Bandara <140834328+sajith23420@users.noreply.github.com>
2026-03-04 16:25:12 +0530

    Delete Payment_Service directory

--- Payment_Service/src/main/java/com/event/payment/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.event.payment.repository;
-
-import com.event.payment.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
