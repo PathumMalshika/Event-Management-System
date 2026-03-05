e8a8efb2d9fc84e0da848567847b31b580e4c4d9
Lakshan Lakshitha <134108423+LakshanWanniarachchi@users.noreply.github.com>
2026-03-05 10:16:39 +0530

    add my sql connection

---
+++ Feedback_Service/src/main/java/com/feedback_service/service/FeedbackServiceImplimnetation.java
@@ -0,0 +1,48 @@
+package com.feedback_service.service;
+
+import com.feedback_service.model.Feedback;
+import com.feedback_service.repository.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class FeedbackServiceImplimnetation implements Feedbackservice{
+
+
+    private FeedbackRepository feedbackRepository;
+
+    @Override
+    public Feedback saveFeedback(Feedback feedback) {
+        System.out.println("User ID: " + feedback.getUserId());
+        System.out.println("Event ID: " + feedback.getEventId());
+        System.out.println("Comment: " + feedback.getComment());
+        return feedbackRepository.save(feedback);
+    }
+
+    @Override
+    public List<Feedback> GetAllFeedback() {
+        return  feedbackRepository.findAll();
+    }
+
+    @Override
+    public Feedback GetFeedbackById(Long id) {
+        return feedbackRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Feedback UpdateFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    @Override
+    public void DeleteFeedback(Long id) {
+
+        feedbackRepository.deleteById(id);
+
+    }
+
+    @Override
+    public List<Feedback> getFeedbackByUserId(Long userid) {
+        return feedbackRepository.findByUserId(userid);
+    }
+}
